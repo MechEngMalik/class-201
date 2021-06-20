6823e4e3d7b30a4fbac8bc27ac71fa4d79c746b7
MechEngMalik <mecheng.malik93@gmail.com>
2021-06-20 19:15:52 +0300

    finishing

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+let userName = prompt('what is your name');
+
+console.log(userName);
+
+let userAge = prompt('what is your age?');
+
+console.log(userAge);
+
+
+let userFav = prompt('who is your favorite player?');
+
+console.log(userFav);
+
+let userWaigth = prompt('what is your favorite club?');
+
+console.log(userWaigth);
+
+let info = userName + userAge + userFav + userWaigth;
+
+
+
+alert(info)
